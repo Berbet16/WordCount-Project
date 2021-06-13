a43b8f84591327de05f9ee1e903954d21cf13ee1
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-06-13 12:48:00 +0300

    The end

--- WordCount/views.py
+++ WordCount/views.py
@@ -2,15 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import operator
 import string
+import re
 
 def home(request):
     return render(request, 'home.html')
 
-def count2(request):
-    return render(request, 'count2.html')
-
 def count(request):
     fullText = request.GET['fullText']
+    sentenceslist = re.split(r'[.!?]+', fullText)
+
     lowerFullText = fullText.lower()
     newFullText = ""
 
@@ -19,7 +19,6 @@ def count(request):
             newFullText += i
 
     wordlist = newFullText.split()
-    sentenceslist = fullText.split(".")
 
     wordDictionary = {}
 
@@ -28,7 +27,7 @@ def count(request):
             #Increase
             wordDictionary[word] += 1
         else:
-            #Add to the dicitonary
+            #Add to the dictionary
             wordDictionary[word] = 1
     sort_wordDictionary = sorted(wordDictionary.items(), key=lambda x: (x[1], x[0]), reverse=True)
     return render(request, 'count.html', {'fullText':fullText, 'count':len(wordlist), 'wordDictionary': sort_wordDictionary, 'sentencesCount':len(sentenceslist)-1})
